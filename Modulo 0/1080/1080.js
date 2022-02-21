eabf3bc3d450004f04dafb236a2f3d4f490ffcad
xMockingjay <aramburocarol@gmail.com>
2022-02-21 18:01:57 -0300

    1080 finalizado

--- Modulo 0/1080/1080.js
+++ Modulo 0/1080/1080.js
@@ -1,17 +1,16 @@
 const input = require('fs').readFileSync('./stdin', 'utf8');
 const lines = input.split(' ');
-console.log(lines);
 
 const tamanhoVetor = lines.length;
 let maior = 0;
 let posicao = 0;
 
 for (let i = 0; i < tamanhoVetor; i++) {
-    if (lines[i] > maior) {
-        maior = lines[i];
+    let parsedNumber = parseFloat(lines[i]);
+    if (parsedNumber > maior) {        
+        maior = parsedNumber;
         posicao = i + 1;
-    }
+    }    
 }
-
 console.log(`${maior}`);
 console.log(`${posicao}`);
