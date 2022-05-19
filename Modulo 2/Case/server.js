429736e615ed948ae9074599f1698e4073a24a60
Vincenzo G Neto <vincenzovgn@gmail.com>
2022-05-19 15:17:32 -0300

    sugestão de melhoria case 2

--- Modulo 2/Case/server.js
+++ Modulo 2/Case/server.js
@@ -1,5 +1,8 @@
-import app from './src/app.js';
-
+import app from './src/app.js'; // esta sendo importado um arquivo que não existe, será que não foi feito push?
+/**
+ * Afim de curiosidade, geralmente a comunidade usa a porta 80 e 8080 para serviços web, front-end ou as vezes banco de dados, mais isso não é regra.
+ * No backend a galera curti usar faixas de 3000, 4000 e 5000, mas também não é regra
+ */
 const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
--- Modulo 2/Case/src/routes/merchantRoutes.js
+++ Modulo 2/Case/src/routes/merchantRoutes.js
@@ -2,7 +2,11 @@ import express from "express";
 import MerchantController from "../controllers/merchantController.js";
 
 const router = express.Router();
-
+/**
+ * Evite o uso de underline para rotas.
+ * Por convensão a comunidade geralmente usa quebec-case e outros usam camelCase. Sugiro o uso de quebec-case.
+ * Mas abaixo estou adicionando uma melhor nomeclatura para as rotas que pode ser vista em: https://github.com/WhiteHouse/api-standards#pragmatic-rest
+ */
 router 
     .get('/merchant', MerchantController.readMerchants)
     .get('/merchant/:id', MerchantController.readMerchantById)
@@ -10,9 +14,9 @@ router
     .post('/merchant', MerchantController.createMerchant)
 
     .put('/merchant/:id', MerchantController.updateMerchant)
-    .put('/merchant_message/:id', MerchantController.sendMessage)
-    .put('/merchant_status/:id', MerchantController.controllStatus)
-    .put('/merchant_categorie/:id', MerchantController.controllCategorie)
+    .put('/merchant/message/:id', MerchantController.sendMessage)
+    .put('/merchant/status/:id', MerchantController.controllStatus)
+    .put('/merchant/categorie/:id', MerchantController.controllCategorie)
 
     .delete('/merchant/:id', MerchantController.deleteMerchant)
 
